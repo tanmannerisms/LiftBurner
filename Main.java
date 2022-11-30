d4d9a1a3fc8a39ccd213f0af160dd820e8d7cb83
tmills2000 <tanner.mills2000@gmail.com>
2022-11-29 20:09:34 -0700

    Just the start

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+import pkg.Excercise;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Excercise BBbenchPress = new Excercise();
+		Excercise.Type jones = Excercise.Type.PUSH;
+		System.out.println(jones);
+	}
+
+	
+}
---
+++ pkg/Excercise.java
@@ -0,0 +1,14 @@
+package pkg;
+
+public class Excercise {
+	
+	public enum Routine {
+		PUSH,
+		PULL,
+		LEGS
+	}
+
+	public void SetRoutine(Routine routine) {
+		Routine this.routine = Routine.routine
+}	
+
