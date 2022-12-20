77a058f25949ad559484553c22745ff4422c770d
Tanner Mills <tanner.mills2000@gmail.com>
2022-12-19 20:41:27 -0700

    Added benchpress subclass to the exercise class. Began work on workDone method to calculate calories

--- pkg/Calculations.java
+++ pkg/Calculations.java
@@ -18,5 +18,16 @@ public class Calculations {
 		return (length * distance);
 	}
 
-	
+	public static double getWork(double force, double distance) {
+		/* Work = Force * Distance when the force is parallel to the displacement of the object being moved.
+		 Work = Force * Distance * cos(theta) when the force is not parallel, where theta is the angle between
+		 the direction of motion of the object and the direction of force acting upon it.
+		 The definition of work require there to be some displacement of the object that a force is acting
+		 upon. In this scenario, I should be taking the suspension of a weight into consideration and the
+		 the amount of 'work' that is required to be uphold said weight without a shift in the position of it.
+		 This should simply be a measure of the length in time that the weight is not moving, multiplied by
+		 the weight of it. This should give an idea of the amount of work that is required.
+		*/
+		return (force * distance);
+	}
 }
--- pkg/Excercise.java
+++ pkg/Excercise.java
@@ -1,11 +1,30 @@
 package pkg;
 
 public class Excercise {
+
+	protected int reps;
+
 	
-	public enum Routine {
+	public enum ROUTINE {
 		PUSH,
 		PULL,
 		LEGS
 	}
 
+	public enum TYPE {
+		SINGLE,
+		DOUBLE
+	}
+
 }	
+
+
+abstract class BenchPress extends Excercise {
+	BenchPress.ROUTINE PUSH;
+	BenchPress.TYPE DOUBLE;
+
+
+	public void setReps(int reps) {
+		this.reps = reps;
+	}
+}
